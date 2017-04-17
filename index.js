8540580256696f3269d6d1fe9143bc47c14a07d0
Brent Lind <brentlind@mac.com>
2017-04-17 12:34:58 -0500

    a

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/web')); //__dir and not _dir
 var port = 8000; // you can use any port
-app.listen(port);
+app.listen(process.env.PORT || port);
 console.log('Node Server Online at port: ' + port);
 
 //--------------------------------------------------
